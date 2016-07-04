593ab26df23d3aae3208b53665a34ca9e22e4419
unknown <>
2016-07-04 12:20:35 +0800

    部分合并

--- MoocPlatform/Modules/Manage/Action/AdminCourseAction.class.php
+++ MoocPlatform/Modules/Manage/Action/AdminCourseAction.class.php
@@ -21,30 +21,43 @@ class AdminCourseAction extends Action{
     public function courseInfo(){
         $courseID = $_POST['CourseID'];
         $db = M('course');
+
+        $courseinfo = $db
+            ->join('department ON course.CourseDep = department.DepartmentID')
+            ->where('course.CourseID='.$courseID)
+            ->getField('course.CourseID,course.CourseName,department.DepartmentName,course.isOpen');
+        $this->assign('courseinfo',$courseinfo);
+
         $courseTeacher = $db
             ->join('courseteacher ON course.CourseID = courseteacher.CourseID')
             ->join('teacher ON teacher.TeaID = courseteacher.TeacherID')
-            ->select();
+            ->where('course.CourseID='.$courseID)
+            ->getField('teacher.TeaName');
+            //->select();
         $this->assign('courseTeacher',$courseTeacher);
 
         $courseStudent = $db
             ->join('coursestudent ON course.CourseID = coursestudent.CourseID')
             ->join('student ON student.StuID = coursestudent.StudentID')
+            ->join('department ON course.CourseDep = department.DepartmentID')
             ->where('course.CourseID='.$courseID)
-            ->select();
+            ->getField('student.StuID,student.StuName,student.Email,department.DepartmentName,student.Grade');
+            //->select();
         $this->assign('courseStudent',$courseStudent);
 
         $courseSchedule = $db
             ->join('courseschedule ON course.CourseID = courseschedule.CourseID')
             ->where('course.CourseID='.$courseID)
             ->select();
-        $this->assign('courseStudent',$courseStudent);
+        $this->assign('courseSchedule',$courseSchedule);
 
-        dump($courseSchedule);
+        //dump($courseSchedule);
+        //$this->display('AdminCourse/courseList_1');
+        $this->display();
     }
 
-    public function hello(){
-        echo 'hello';
+    public function courseNewInfo(){
+        $this->display();
     }
 }
 
