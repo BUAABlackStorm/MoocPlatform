87ebf12929e420edd07057a1ff160f470766730e
unknown <>
2016-07-05 15:55:56 +0800

    教务学期管理完成

--- MoocPlatform/Modules/Manage/Action/AdminTermAction.class.php
+++ MoocPlatform/Modules/Manage/Action/AdminTermAction.class.php
@@ -27,7 +27,35 @@ class AdminTermAction extends Action{
     }
     
     public function newTerm(){
+        $term = M('term');
+        $nowterm = $term -> where('term.TermStatus = 1') -> select();
+
+        if($nowterm != null)
+            $this->error('当前已有开始学期，无法创建新学期','getTermList');
+        
         $this -> display('AdminTerm/preferences');
+    }
+    
+    public function addTerm(){
+        $termBegin = $_POST['termbegin'];
+        $termEnd = $_POST['termend'];
+        $termweeknum = $_POST['times'];
+        $termname = $_POST['termname'];
+
+        dump($termBegin);
+
+        if($termBegin == null || $termEnd == null ||$termweeknum == null || $termname == null)
+            $this->error('信息不能为空','AdminTerm/newTerm');
+
+        $term = M('term');
+        $data['TermName'] = $termname;
+        $data['TermWeeks'] = $termweeknum;
+        $data['TermStart'] = $termBegin;
+        $data['TermEnd'] = $termEnd;
+        $data['TermStatus'] = 1;
+        $term -> add($data);
+
+        $this->success('新增学期成功', 'getTermList');
     }
 
     
