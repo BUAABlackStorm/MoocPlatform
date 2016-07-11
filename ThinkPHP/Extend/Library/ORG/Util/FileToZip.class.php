c052ab88b66c1c18f6a61a5db213f66a2be44c74
xcgxg <xcgxg@users.noreply.github.com>
2016-07-11 17:14:23 +0800

    Update FileToZip.class.php

--- ThinkPHP/Extend/Library/ORG/Util/FileToZip.class.php
+++ ThinkPHP/Extend/Library/ORG/Util/FileToZip.class.php
@@ -57,8 +57,6 @@ class traverseDir{
 
             $dw=new download($zipname,""); //下载文件
 
-            ob_clean();
-
             $dw->getfiles();
             unlink($zipname); //下载完成后要进行删除  
         }
@@ -96,30 +94,33 @@ class download{
     public function getfiles(){
         //检查文件是否存在
         if (file_exists($this->_filepath)){
-            //打开文件
-            $file = fopen($this->_filepath,"r");
-            //返回的文件类型
-            Header("Content-type: application/octet-stream");
-            //按照字节大小返回
-            Header("Accept-Ranges: bytes");
-            //返回文件的大小
-            Header("Accept-Length: ".filesize($this->_filepath));
-            //这里对客户端的弹出对话框，对应的文件名
-            Header("Content-Disposition: attachment; filename=".urlencode($this->_filename));
-            //修改之前，一次性将数据传输给客户端
-            echo fread($file, filesize($this->_filepath));
-            //修改之后，一次只传输1024个字节的数据给客户端
-            //向客户端回送数据
-            $buffer=1024;//
-            //判断文件是否读完
-            while (!feof($file)) {
-                //将文件读入内存
-                $file_data=fread($file,$buffer);
-                //每次向客户端回送1024个字节的数据
-                echo $file_data;
-            }
+            import('ORG.Net.Http');
+            ob_clean();
+            Http::download($this->_filepath,$this->_filename);
+            // //打开文件
+            // $file = fopen($this->_filepath,"r");
+            // //返回的文件类型
+            // Header("Content-type: application/octet-stream");
+            // //按照字节大小返回
+            // Header("Accept-Ranges: bytes");
+            // //返回文件的大小
+            // Header("Accept-Length: ".filesize($this->_filepath));
+            // //这里对客户端的弹出对话框，对应的文件名
+            // Header("Content-Disposition: attachment; filename=".$this->_filename);
+            // //修改之前，一次性将数据传输给客户端
+            // echo fread($file, filesize($this->_filepath));
+            // //修改之后，一次只传输1024个字节的数据给客户端
+            // //向客户端回送数据
+            // $buffer=1024;//
+            // //判断文件是否读完
+            // while (!feof($file)) {
+            //     //将文件读入内存
+            //     $file_data=fread($file,$buffer);
+            //     //每次向客户端回送1024个字节的数据
+            //     echo $file_data;
+            // }
             
-            fclose($file);
+            // fclose($file);
         }else {
             echo "<script>alert('对不起,您要下载的文件不存在');</script>";
         }
